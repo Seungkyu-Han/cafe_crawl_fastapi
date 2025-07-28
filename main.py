d7b88aac9ba1b01eda346cf753b04ac2304d7a87
Seungkyu-Han <trust1204@gmail.com>
2025-07-29 08:06:47 +0900

    feat: cafe crawl api

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 import cafes.cafeDto
+from cafes.cafeService import retrieve_menu
 
 app = FastAPI()
 
 @app.post("/cafe")
-async def crawl_cafe_menu(cafe_crawl_req: cafes.cafeDto.CafeCrawlReq) -> cafes.cafeDto.CafeCrawlRes:
-    return
+async def crawl_cafe_menu_api(cafe_crawl_req: cafes.cafeDto.CafeCrawlReq) -> cafes.cafeDto.CafeCrawlRes:
+
+    return retrieve_menu(cafe_type=cafe_crawl_req.cafeType, is_crawl_img=cafe_crawl_req.isCrawlImg)
