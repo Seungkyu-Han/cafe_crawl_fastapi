5ee575375122343f1c93c50c7f6bff2f53e83baf
Seungkyu-Han <trust1204@gmail.com>
2025-07-29 00:04:49 +0900

    feat: cafe crawl api parameter

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
+import cafes.cafeDto
+
 app = FastAPI()
 
 @app.post("/cafe")
-async def crawl_cafe_menu(name: str):
-    return {"message": f"Hello {name}"}
+async def crawl_cafe_menu(cafe_crawl_req: cafes.cafeDto.CafeCrawlReq) -> cafes.cafeDto.CafeCrawlRes:
+    print(cafe_crawl_req)
+    return None
